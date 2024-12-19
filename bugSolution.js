3368742231344ce6a85018c9a84f1810c86c2827
BugHunter-X <askzenai@gmail.com>
2024-12-19 16:22:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,7 @@
+```javascript
+// ... your Tailwind CSS classes
+<div class="bg-gray-100 p-4">
+  <p class="text-xl font-bold">This text should be larger</p>
+  <p class="text-base">This text is normal size.</p>
+</div>
+```
